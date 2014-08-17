8c2306e5244bc344728d1c547506c944a5371226
robinHoover <robin.hoover@gmail.com>
2014-08-17 12:41:06 -0400

    Fix Analytics

--- app/scripts/services/analytics-google.js
+++ app/scripts/services/analytics-google.js
@@ -10,20 +10,13 @@
 
 angular
   .module('vtbtApp')
-  .factory('analyticsGoogle', function () {
+  .factory('analyticsGoogle', function ($window) {
 
         return {
-            logPageLoad: function ($window, scope, absoluteUrl, locationPath) {
+            logPageLoad: function (scope, absoluteUrl, locationPath) {
 
                 if (absoluteUrl.indexOf('vtbeertrail') > 0) {
                     $window._gaq.push(['_trackPageview', locationPath]);
-                    // scope.$on('$viewContentLoaded', function (event) {
-                    //     $window._gaq.push(['_trackPageview', locationPath]);
-                    //     console.log('tracked!');
-                    // });
-                //      else {
-                //     console.log('URL not tracked:', locationPath);
-                // }
                 }
             }
         };
