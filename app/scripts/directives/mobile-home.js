8fb9a7d28397aff8a33481a7c58511e9ea0f0de2
robinHoover <robin.hoover@gmail.com>
2014-11-29 10:23:16 -0500

    Improved Home BG

--- app/scripts/directives/mobile-home.js
+++ app/scripts/directives/mobile-home.js
@@ -9,21 +9,32 @@
 
 angular
     .module('vtbtApp')
-    .directive('mobileHome', function ($location) {
+    .directive('mobileHome', function ($location, $timeout) {
 
         return {
 
             restrict: 'A',
             scope: {},
 
-            link: function (scope, element) {
-                scope.$on('$locationChangeSuccess', function () {
-                    if ($location.path() === '/') {
-                        element.addClass('body-small-home');
-                    } else {
-                        element.removeClass('body-small-home');
-                    }
-                });
+            link: function (scope, element, attrs) {
+                var bodyResult = getComputedStyle(element[0], ':after').content;
+                bodyResult = bodyResult.replace(/"/g,''); //Because Firefox keeps quotes from content
+
+                switch (bodyResult) {
+                    case 'phone' :
+                        scope.$on('$locationChangeSuccess', function () {
+                            if ($location.path() === '/') {
+                                $timeout(function(){
+                                    element.addClass('body-small-home');
+                                }, 500);
+                            } else {
+                                $timeout(function(){
+                                    element.removeClass('body-small-home');
+                                }, 500);
+                            }
+                        });
+                    break;
+                }
             }
         };
     });
